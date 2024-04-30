b56978c8200e771299b9eaae95a8737eb794b58e
Luke Sheakley <luke.sheakley@gmail.com>
2024-04-30 11:41:10 -0400

    04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...elem) {
+    for(let i = 0; i < elem.length; i++){
+        while(arr.includes(elem[i])){
+            arr.splice(arr.indexOf(elem[i]),1)
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
