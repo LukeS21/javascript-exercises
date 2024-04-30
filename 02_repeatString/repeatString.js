70abfa2f8e59077b9d777c79a03129cd4421105d
Luke Sheakley <luke.sheakley@gmail.com>
2024-04-30 11:42:39 -0400

    updated to 4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(input, times) {
+    let returnString = "";
+    for(let i = 0; i<times; i++){
+        returnString += input;
+    }
+    return returnString;
 };
 
 // Do not edit below this line
